724e153c1b38744ce618284afb67c189ae1443cc
Ashish-Shukla-salesforce <85156155+Ashish-Shukla-salesforce@users.noreply.github.com>
2021-06-01 13:31:56 +0530

    Add files via upload

---
+++ src/admin.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author user
+ */
+public class admin {
+    public static String Adminname;
+
+    public static String getAdminname() {
+        return Adminname;
+    }
+
+    public static void setAdminname(String Adminname) {
+        admin.Adminname = Adminname;
+    }
+    
+    
+}
